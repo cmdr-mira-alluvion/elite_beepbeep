3c63b54560183c12c6c71c1477eaf6b84ac92e35
cmdr-mira-alluvion <32249580+cmdr-mira-alluvion@users.noreply.github.com>
2018-07-20 19:52:10 -0700

    Logging functionality done and tested
    Also some code consistency cleanups

--- elite_beepbeep.ps1
+++ elite_beepbeep.ps1
@@ -1,4 +1,4 @@
-$scriptVersion = "20180720_025745"
+$scriptVersion = "20180720_195050"
 
 #Seeing a warning message that says "Security Warning Run only scripts that you trust."?
 #Try this to fix it:
@@ -17,7 +17,7 @@ $readNames = $false
 $customSound = $false
 
 #custom "beep" sound path
-$customSoundPath = '.\seatbelt.wav'
+$customSoundPath = ".\seatbelt.wav"
 
 #how often to check the cmdrHistory file (in seconds), file watcher eventing seems to be squicky and inconsistent
 #may benefit from a higher value since cmdrHistory isn't as immediately-responsive as netlog
@@ -30,15 +30,15 @@ $pollForever = $true
 $updateInterval = 60
 
 #these should never change, but just in case, it's in the config section
-$folder = (Get-ChildItem Env:LOCALAPPDATA).Value + '\Frontier Developments\Elite Dangerous\CommanderHistory'
-$filter = '*.cmdrHistory'
+$folder = (Get-ChildItem Env:LOCALAPPDATA).Value + "\Frontier Developments\Elite Dangerous\CommanderHistory"
+$filter = "*.cmdrHistory"
 #TODO: handle potential multi-user scenarios here
 
 #whether or not to log to file into journal log directory
 $logToFile = $true
 
 #url to fetch cmdr ID -> name from
-$definitions = 'https://kncg.pw/hg4f3QL.txt'
+$definitions = "https://kncg.pw/hg4f3QL.txt"
 
 ########## CONFIGURATION ##########
 
@@ -52,7 +52,7 @@ Function Get-UnixTime() {
 #returns Interactions array from cmdrHistory file
 Function Get-CmdrHistory() {
     Try {
-        Return (Get-Content (Join-Path -Path $folder -ChildPath (Get-ChildItem -Path $folder -Filter $filter | Select -Last 1).Name) | ConvertFrom-Json).'Interactions'
+        Return (Get-Content (Join-Path -Path $folder -ChildPath (Get-ChildItem -Path $folder -Filter $filter | Select -Last 1).Name) | ConvertFrom-Json)."Interactions"
     } Catch {
         Return $null
     }
@@ -60,8 +60,8 @@ Function Get-CmdrHistory() {
 
 #returns cmdr ID from journal log if they aren't in the defs
 Function Get-JournalName() {
-    $journaldir = (Get-ChildItem Env:USERPROFILE).Value + '\Saved Games\Frontier Developments\Elite Dangerous'
-    $lastlog = Get-Content (Join-Path -Path $journaldir -ChildPath (Get-ChildItem -Path $journaldir -Filter 'Journal.*.log' | Select -Last 1).name) | ConvertFrom-Json
+    $journaldir = (Get-ChildItem Env:USERPROFILE).Value + "\Saved Games\Frontier Developments\Elite Dangerous"
+    $lastlog = Get-Content (Join-Path -Path $journaldir -ChildPath (Get-ChildItem -Path $journaldir -Filter "Journal.*.log" | Select -Last 1).name) | ConvertFrom-Json
     foreach ($event in $lastlog) {
         if ($event.event -eq "Commander") {
             Return $event.name
@@ -74,10 +74,10 @@ Function Get-JournalName() {
 Function Get-IDToNames {
     #fetch definitions from internet if configured
     $cmdrs = $null
-    If ($definitions -ne '') {
+    If ($definitions -ne "") {
         Try {
             #grab latest defs from this url and convert into psobject from json
-            $progressPreference = 'silentlyContinue'
+            $progressPreference = "silentlyContinue"
             $cmdrs = Invoke-WebRequest -Uri $definitions -Method Get -UseBasicParsing -ErrorAction Stop | ConvertFrom-Json
             
             #exit if it's the same version
@@ -91,10 +91,10 @@ Function Get-IDToNames {
             $count = ($cmdrs | Get-Member -MemberType NoteProperty).count - 2
             
             #emit info to console
-            #Write-Host -ForegroundColor Green "Loaded $count ID->CMDR definitions -- last updated $lastDefs"
+            #"Loaded $count ID->CMDR definitions -- last updated $lastDefs" | Write-Host -ForegroundColor Green
         } Catch {
             #something went wrong -- exit and try again later
-            Write-Host -ForegroundColor Red "Could not fetch load ID->CMDR definitions. Please try again later."
+            "Could not fetch load ID->CMDR definitions. Please try again later." | Write-Host -ForegroundColor Red
             Break
             $cmdrs = $null
         }
@@ -106,12 +106,12 @@ Function Get-IDToNames {
 Function Out-AlertBeep($last, $newNameInput) {
     #enforced minimum cooldown between beeps
     $now = (Get-UnixTime)
-    $type = 'double'
+    $type = "double"
     
     If (($sound -eq $true) -and (($now - $last) -gt $cooldown)) {
         $last = $now
         
-        If ($type -eq 'double') {
+        If ($type -eq "double") {
             #TODO: Move readnames into $type, fully change to param, pass 'read' in mainloop bead on $readNames value
             If ($readNames -eq $false) {
                 #TODO: Add file existence check here for custom sound
@@ -125,13 +125,14 @@ Function Out-AlertBeep($last, $newNameInput) {
             } Else {
                 #Read Commander Name
                 $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
-                $speak.Speak($newNameInput)
+                $speak.SelectVoiceByHints("Female")
+                $speak.SpeakAsync($newNameInput) | Out-Null
             }
-        } ElseIf ($type -eq 'ascend') {
+        } ElseIf ($type -eq "ascend") {
             #ascending A6-E7 beeps
             [Console]::Beep(1760.000000,200)
             [Console]::Beep(2637.020455,200)
-        } ElseIf ($type -eq 'descend') {
+        } ElseIf ($type -eq "descend") {
             #descending E7-A6 beeps
             [Console]::Beep(2637.020455,200)
             [Console]::Beep(1760.000000,200)
@@ -164,7 +165,9 @@ $lastDefs = 0
 $instance = New-Object System.Collections.ArrayList
 
 #initialize speech synthesizer
-Add-Type -AssemblyName System.Speech
+If ($readNames -eq $true) {
+    Add-Type -AssemblyName System.Speech
+}
 
 ########## INIT ##########
 
@@ -173,8 +176,8 @@ Get-Variable true | Out-Default
 Clear-Host
 
 #logfile init
-$logFile = (Get-ChildItem Env:USERPROFILE).Value + '\Saved Games\Frontier Developments\Elite Dangerous\Beepbeep.' + (Get-UnixTime) + '.log'
-$logBuffer = ''
+$logFile = (Get-ChildItem Env:USERPROFILE).Value + "\Saved Games\Frontier Developments\Elite Dangerous\Beepbeep." + (Get-UnixTime) + ".log"
+$logBuffer = ""
 
 #initial defs fetch
 $cmdrs = Get-IDToNames
@@ -182,24 +185,24 @@ $cmdrs = Get-IDToNames
 #spit out current user's name entry if found, ID number otherwise
 $currentID = ((Get-ChildItem -Path $folder -Filter $filter | Select -Last 1).Name) -Replace "Commander(\d+)\.cmdrHistory", '$1'
 $currentName = If ($cmdrs.$currentID) { $cmdrs.$currentID } Else { $currentID + " '" + (Get-JournalName) + "'" }
-If ($definitions -ne '') { $definitions += '?s=' + [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($currentName)) }
+If ($definitions -ne "") { $definitions += "?s=" + [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($currentName)) }
 "ID: $currentName`n" | Tee-Object -Variable logBuffer | Write-Host -ForegroundColor Green
 
 #exit instructions
-Write-Host -ForegroundColor Red "Press Ctrl+C to exit...`n"
+"Press Ctrl+C to exit...`n" | Write-Host -ForegroundColor Red
 
 #slurp up cmdrHistory file every polling interval, see which entries are new and spit them out
 While ($pollForever) {
     #flush log buffer if it's enabled
-    if (($logFile) -and ($logBuffer -ne '')) {
-        Out-File -InputObject $logBuffer -FilePath $logFile -Encoding UTF8 -Append
-        $logBuffer = ''
+    if (($logFile) -and ($logBuffer -ne "")) {
+        Out-File -InputObject $logBuffer -FilePath $logFile -Encoding UTF8 -Append -NoNewline
+        $logBuffer = ""
     }
     
     #scoop up history file as JSON, extract top-level Interactions array, parse through cmdrHistory for new entries
     Get-CmdrHistory | ForEach-Object {
-        $epoch = $_.'Epoch'
-        $id = $_.'CommanderID'
+        $epoch = $_."Epoch"
+        $id = $_."CommanderID"
         $name = If ($cmdrs.$id) { $cmdrs.$id } Else { $id }
         
         #only emit a beep and a text line for new entries
@@ -211,26 +214,26 @@ While ($pollForever) {
             #if cmdr exists in the instance already, this beep is actually outbound rather than inbound
             If ($instance.BinarySearch($id) -lt 0) {
                 $null = $instance.Add($id)
-                $direction = @('Red', '  --->')
+                $direction = @("Red", "  --->")
                 
                 #threshold-gated beep because this is an _incoming_ contact
                 $lastBeep = Out-AlertBeep $lastBeep $name
             } Else {
                 While ($instance.BinarySearch($id) -ge 0) { $instance.Remove($id) }
-                $direction = @('Green', '<---  ')
+                $direction = @("Green", "<---  ")
                 
                 #no beep for outbound
             }
             
             ("[{0}] {1} {2}" -f $date, $direction[1], $name) | Tee-Object -Variable lineBuffer | Write-Host -ForegroundColor $direction[0]
-            $logBuffer += $lineBuffer + '`n'
+            $logBuffer += $lineBuffer + "`n"
         }
         $firstRun = $false
     }
     
     #update cmdr defs if the timer says we need to
     If (((Get-UnixTime) % $updateInterval) -eq $updateOffset) {
-        $cmdrs2 = Get-IDToNames
+        $cmdrs2 = (Get-IDToNames)
         If ($cmdrs2) { $cmdrs = $cmdrs2 }
     }
     
